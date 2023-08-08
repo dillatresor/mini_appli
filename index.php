6b4ca9382407fdba01f3c7682845cb79e9f18096
DILLA TRESOR <dillatresor3@gmail.com>
2023-08-08 14:23:10 +0000

    ajout

--- index.php
+++ index.php
@@ -8,27 +8,30 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <img style="max-width: 30%;" src="images/logo simplon.png" alt="Description de l'image" class="img-fluid">
+    <div class="container-fluid" style="background-color: rgb(216, 5, 5);">
+        <img style="max-width: 26%;" src="images/logo simplon.png" alt="Description de l'image" class="img-fluid,">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">ACCUEIL</a>
+            <button type="button" class="btn btn-secondary btn-lg m-4">ACCUEIL</button>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">APPRENANTS</a>
+            <button type="button" class="btn btn-secondary btn-lg m-4">APPRENANTS</button>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">TUTEURS</a>
+            <button type="button" class="btn btn-secondary btn-lg m-4">TUTEURS</button>
           </li>
-        
         </ul>
         <img style="max-width: 30%;" src="images/logo hcr.png" alt="Description de l'image" class="img-fluid">
+       
       </div>
     </div>
   </nav>
+  <div class="bg-light p-4" style="">
+    <h2>BIENVENU DANS GESTION DES APPRENANTS</h2>
+  </div>
 </body>
 </html>
