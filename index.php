750d4beadf4ea2169e4fbeb8cb512fd33983ef10
DILLA TRESOR <dillatresor3@gmail.com>
2023-08-09 11:23:53 +0000

    ajout new

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid" style="background-color: rgb(216, 5, 5);">
+<nav class="m-5 navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid bg-danger">
         <img style="max-width: 26%;" src="images/logo simplon.png" alt="Description de l'image" class="img-fluid,">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -16,22 +16,31 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <button type="button" class="btn btn-secondary btn-lg m-4">ACCUEIL</button>
+            <button type="button" class="btn btn-light btn-lg m-4">ACCUEIL</button>
           </li>
           <li class="nav-item">
-            <button type="button" class="btn btn-secondary btn-lg m-4">APPRENANTS</button>
+            <button type="button" class="btn btn-light btn-lg m-4">APPRENANTS</button>
           </li>
           <li class="nav-item">
-            <button type="button" class="btn btn-secondary btn-lg m-4">TUTEURS</button>
+            <button type="button" class="btn btn-light btn-lg m-4">TUTEURS</button>
           </li>
         </ul>
-        <img style="max-width: 30%;" src="images/logo hcr.png" alt="Description de l'image" class="img-fluid">
+        <img style="max-width: 26%;" src="images/logo hcr.png" alt="Description de l'image" class="img-fluid,">
        
       </div>
     </div>
   </nav>
-  <div class="bg-light p-4" style="">
-    <h2>BIENVENU DANS GESTION DES APPRENANTS</h2>
-  </div>
+               <div class="text-center" style="font-family: Algerian">
+                        <h2>BIENVENUE DANS L'ANNUAIRE DES APPRENANTS</h2>
+              </div>      
+             <div class="contenair p-4 m-5 bg-danger">       
+             <div> 
+                      <img src="images/photo.jpeg" alt=" "style="width: 100%; height: 500px ;">
+             </div>
+             </div>   
+
+              <div class="m-5 bg-danger text-center width:20% text-light"> 
+                       <h2>COPYRIGHT SIMPLON 2020</h2>
+              </div>
 </body>
 </html>
